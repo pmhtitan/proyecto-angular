dfe578116f5a598bd0113809010d87c06244d9b6
PMH <pmhtitan@gmail.com>
2021-05-27 19:21:08 +0200

    added assets, bootstrap, components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// rutas
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CreateComponent } from './components/create/create.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+
+const routes: Routes = [
+
+  { path: '', pathMatch: 'full', redirectTo: 'projects' },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'create', component: CreateComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
